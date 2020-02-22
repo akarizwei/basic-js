327e12b05da52c45a74c6cef4435b2ba71aef8d5
Akarizwei <andy.b.sky@yandex.ru>
2020-02-23 00:43:01 +0400

    feat: add solution dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,9 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+    if (members instanceof Array){
+        let arr = members;
+        arr = arr.filter(item => (typeof item)=='string').map(item => item.split('').find(item => item.match(/[a-zA-Z]+/g))).filter(item => (typeof item)=='string');
+        arr = arr.map(item => item.toUpperCase()).sort().join('');
+        return arr
+    }
+    return false
 };
